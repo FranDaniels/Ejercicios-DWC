f9d95cabead90788236ad30e92c9939061856bab
Francisco <coheteomega@gmail.com>
2023-09-29 09:28:33 +0200

    Realizar ejercicio Eventos

---
+++ Eventos/app.js
@@ -0,0 +1,37 @@
+//Francisco Álvarez Bellón
+const boton=document.getElementById('boton')
+const mensaje=document.getElementById('mensajeError')
+
+let comprobarIban = /^(ES76|ES78)$/;
+let comprobarCuenta = /^[0-9]{20}$/;
+let comprobarNumeroTarjeta = /^[0-9]{16}$/;
+let comprobarCvv = /^[0-9]{3}$/;
+
+boton.addEventListener('click', function(){
+const iban=document.getElementById('iban').value
+const cuenta=document.getElementById('cuenta').value
+const numTarjeta=document.getElementById('numeroTarjeta').value
+const cvv=document.getElementById('CVV').value
+
+    mensaje.textContent=''
+    if (iban.length!==4||!comprobarIban.test(iban)) {
+        mensaje.textContent='Se necesita que el Iban contenga 4 caracteres y deben ser ES76 o ES78'
+    }else{
+        if(cuenta.length!==20){
+            mensaje.textContent='Se necesita que el numero de cuenta contenga 20 caracteres'
+        }else{ 
+            if(numTarjeta.length!==16||!comprobarNumeroTarjeta.test(numTarjeta)){
+                mensaje.textContent='Número de tarjeta incorrecto'
+            }else{
+                if (cvv.length!==3||!comprobarCvv.test(cvv)) {
+                    mensaje.textContent='El CVV se necesita que contenga 3 numeros'
+                }
+            }
+        }
+    }
+})
+
+const numTarjeta=document.getElementById('numeroTarjeta')
+numTarjeta.addEventListener('change', function(){
+    document.getElementById('CVV').value = ""
+})
